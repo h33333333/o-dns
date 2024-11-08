c23153110b67ea51e6a42c90fd64f0115ddcb237
Dmytro Andriievych <andriievych.dmytro@gmail.com>
2024-11-08 07:22:00 +0200

    chore(o-dns): fix CLI arg

--- crates/o-dns/src/cli.rs
+++ crates/o-dns/src/cli.rs
@@ -11,7 +11,7 @@ pub struct Args {
     pub allowlist_path: Option<PathBuf>,
     #[arg(short('c'), long, value_name = "CONNECTIONS", default_value_t = 5, value_parser = clap::value_parser!(u8).range(1..=10))]
     pub max_parallel_connections: u8,
-    #[arg(short('h'), long, value_name = "ADDR", default_value = "127.0.0.1")]
+    #[arg(short('b'), long, value_name = "ADDR", default_value = "127.0.0.1")]
     pub host: IpAddr,
     #[arg(short('p'), long, value_name = "PORT", default_value_t = 53)]
     pub port: u16,
